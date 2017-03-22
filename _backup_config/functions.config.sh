80d9a352a5919c21356b35eb68da7f0b61f1a0c1
Kai <kai.weidner@gmail.com>
2017-03-22 15:15:53 +0100

    minor changes and cleanup

--- _backup_config/functions.config.sh
+++ _backup_config/functions.config.sh
@@ -10,14 +10,14 @@ make_dir() {
 #"a" for apply and "s" for save
 apply_or_create_metadata() {
 	METASTORE_FILE="$SCRIPT_HOME/metastore_file"
-	if [ -f $METASTORE_FILE ] && ["$1" -eq "s"]
+	if [ -f $METASTORE_FILE ] && [ "$1" -eq "s" ]
 	then
 		rm $METASTORE_FILE
 	fi
 	cd $SCRIPT_HOME
 
 	#apply metastore rights
-	./metastore -$1 -f metastore_file
+	./metastore -"$1" -f metastore_file
 }
 
 # Stop Services defined in SERVICES_TO_STOP
