7e0f22c43e111782a3bd0f0faabcf3d939e4dd94
Kai <kai.weidner@gmail.com>
2019-07-09 18:05:30 +0200

    Update functions.config.sh, removed unnecessary functions.

--- _backup_config/functions.config.sh
+++ _backup_config/functions.config.sh
@@ -7,19 +7,6 @@ make_dir() {
 	fi
 }
 
-#"a" for apply and "s" for save
-apply_or_create_metadata() {
-	METASTORE_FILE="$SCRIPT_HOME/metastore_file"
-	if [ -f $METASTORE_FILE ] && [ "$1" -eq "s" ]
-	then
-		rm $METASTORE_FILE
-	fi
-	cd $SCRIPT_HOME
-
-	#apply metastore rights
-	./metastore -"$1" -f metastore_file
-}
-
 # Stop Services defined in SERVICES_TO_STOP
 start_stop_services() {
 	for s in "${SERVICES_TO_STOP[@]}"
