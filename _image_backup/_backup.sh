f49f63a5511aa7b687713abb7339c4d4283ab560
Kai <kai.weidner@gmail.com>
2022-06-05 11:11:27 +0200

    Image SD Card backup script

---
+++ _image_backup/_backup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ $UID -ne 0 ]; then       # or `id -u`
+    exec sudo -- "$0" "$@"
+fi
+
+target=/media/backup/
+today=$(date +%Y-%m-%d)
+
+device=find /dev/disk/by-id/ -lname '*sdb'
+
+if [[ $device == *"SD_MMC"* ]]; then
+  mkdir -p $target
+  rsync -avR --delete -files-from=:backup_paths.list  "${target}${today}/" --link-dest="${target}last/"
+  ln -nsf "${target}${today}" "${ziel}last"
+fi
+
+echo device is no sd card
