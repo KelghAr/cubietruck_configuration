3c6979ebe40c4fc113053fde3228b05e83e1de19
Kai <kai.weidner@gmail.com>
2022-06-05 11:14:18 +0200

    Update image script

--- _image_backup/_backup.sh
+++ _image_backup/_backup.sh
@@ -9,10 +9,11 @@ today=$(date +%Y-%m-%d)
 
 device=find /dev/disk/by-id/ -lname '*sdb'
 
-if [[ $device == *"SD_MMC"* ]]; then
+if [[ $device == *"SD_MMC"* ]]; then  
   mkdir -p $target
-  rsync -avR --delete -files-from=:backup_paths.list  "${target}${today}/" --link-dest="${target}last/"
-  ln -nsf "${target}${today}" "${ziel}last"
+  mount /dev/sdb $target
+  #rsync -avR --delete -files-from=:backup_paths.list  "${target}${today}/" --link-dest="${target}last/"
+  #ln -nsf "${target}${today}" "${ziel}last"
 fi
 
 echo device is no sd card
