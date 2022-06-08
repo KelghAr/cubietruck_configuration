e48c68fbcbb66f4be041b9b0f63c6eacebe3f8ca
Kai <kelghar@andromeda.home>
2022-06-08 17:25:51 +0200

    finished image bakcup

--- _image_backup/_backup.sh
+++ _image_backup/_backup.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-if [ $(id -u) != "0" ]
-  then
+if [ $(id -u) != "0" ]; then
     echo "Please use sudo"
-  exit
+    exit
 fi
 
 target=/media/backup/
 today=$(date +%Y-%m-%d)
 
-device=find /dev/disk/by-id/ -lname '*sdb'
+device=$(find /dev/disk/by-id/ -lname '*sdd')
 
-if [[ $device == *"SD_MMC"* ]]; then  
+echo $device
+
+if [[ $device == *"usb-Generic"* ]]; then  
   mkdir -p $target
-  mount /dev/sdb $target
-  #rsync -avR --delete -files-from=:backup_paths.list  "${target}${today}/" --link-dest="${target}last/"
+  mount /dev/sdd1 $target
+  rsync -avrR --delete --files-from="backup_paths.list" / "${target}${today}/"
   #ln -nsf "${target}${today}" "${ziel}last"
+  umount $target
+  exit
 fi
 
 echo device is no sd card
